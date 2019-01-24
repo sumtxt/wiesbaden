ff9324416872ac8ac9de9de1f3b5804f9fae0d13
MM <none@none.com>
2019-01-24 16:11:35 +0100

    Added documentation to read_header_genesis().

--- R/read_header_genesis.R
+++ R/read_header_genesis.R
@@ -7,6 +7,7 @@
 #' @param lines number of header lines 
 #' @param locale default encoding is 'windows-1252'
 #' @param replacer a vector that is used as the first K column-names
+#' @param clean_letters make proper variable names? (default: TRUE)
 #' 
 #'   
 #' @details 
@@ -23,8 +24,7 @@
 #'   
 #'	 require(readr)
 #'	 
-#'	 url <- 'https://www-genesis.destatis.de/genesis/online?sequenz=tabelleDownload&selectionname=12411-0004&format=csv'
-#'	 download.file(url, '12411-0004.csv')
+#'	 download_csv(tablename="12411-0004")
 #'	 
 #'	 d <- read_header_genesis('12411-0004.csv', start=6, replacer=c("STAG"))
 #'	 data <- read_csv2('12411-0004.csv', skip=6, n_max=30-6+1, na="-", locale=locale(encoding="windows-1252") )
@@ -35,10 +35,9 @@
 #' 
 #' 
 #' @export
-read_header_genesis <- function(..., start, lines=2, readr_locale=locale(encoding="windows-1252"), replacer=NULL, 
-				clean_letters=T){
+read_header_genesis <- function(..., start, lines=2, readr_locale=locale(encoding="windows-1252"), replacer=NULL, clean_letters=TRUE){
 	h <- read_csv2(..., col_names=FALSE, skip=start-1, n_max=lines, col_types=cols( .default = col_character() ), locale=readr_locale )
-	if(clean_letters==T){
+	if(clean_letters==TRUE){
 		h <- apply(h, 2, function(x) get_character_vec(x) )
 		} else{
 		h <- apply(h, 2, function(x) paste(unlist(na.omit(x), use.names=FALSE), collapse=" "))
