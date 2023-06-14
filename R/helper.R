d0cb69ca8e50a19c639adf9b9d6a838dd0981d3c
mirkoj <mirkoj@mirkoj.xy>
2023-06-14 17:59:11 +0200

    Implemented resapi in set_db and test_login

--- R/helper.R
+++ R/helper.R
@@ -14,13 +14,13 @@ make_genesis <- function(genesis){
 		}
 		else if (genesis['db']=='bm'){
 			genesis <- key_user_pw(genesis,"bildungsmonitoring")
-		}	
+		}
 		else if (genesis['db']=='st'){
 			genesis <- key_user_pw(genesis,"landesdatenbank-st")
-		}	
+		}
 		else if (genesis['db']=='by'){
 			genesis <- key_user_pw(genesis,"landesdatenbank-by")
-		}	
+		}
 		else if (genesis['db']=='de'){
 			genesis <- key_user_pw(genesis,"destatis")
 		}	else {
@@ -32,23 +32,23 @@ make_genesis <- function(genesis){
 
 key_user_pw <- function(genesis,service){
 	genesis["user"] <- as.character(key_list(service=service)['username'])
-	genesis["password"] <- as.character(key_get(service=service, 
+	genesis["password"] <- as.character(key_get(service=service,
 		username=genesis["user"]))
 	return(genesis)
 	}
 
 # genesis_error_check <- function(xml){
-# 	
+#
 # 	if ( length(xml)==0 ) {
 # 		error <- xml_find_all(xml, './/faultstring/text()')
 # 		if ( length(error) !=0 ) stop(as.character(error))
 # 		}
-# 
-# 	if ( length(xml)==1 ){ 
+#
+# 	if ( length(xml)==1 ){
 # 		if ( xml_has_attr(xml, 'nil')==TRUE )	{
 # 			stop("No results found.") }
 # 		}
-# 
+#
 # 	}
 
 readstr_csv <- function(string,skip=0){
@@ -57,25 +57,31 @@ readstr_csv <- function(string,skip=0){
 	return(tab)
 	}
 
-set_db <- function(db){
-	if (db=="nrw") return("https://www.landesdatenbank.nrw.de/ldbnrwws/services/")
-	if (db=="regio") return("https://www.regionalstatistik.de/genesisws/services/")
-	if (db=="de") return("https://www-genesis.destatis.de/genesisWS/web/")
-	if (db=="bm") return("https://www.bildungsmonitoring.de/bildungws/services/")
-	if (db=="st") return("https://genesis.sachsen-anhalt.de/webservice/services/")
-	if (db=="by") return("https://www.statistikdaten.bayern.de/genesisWS/services/")
-	stop("DB: Currently not implemented.")
-	}
+set_db <- function(db, restapi = FALSE){
+
+  if(restapi){
+    if (db=="de") return("https://www-genesis.destatis.de/genesisWS/rest/2020/")
+    stop("DB (rest): Currently not implemented.")
+  } else {
+    if (db=="nrw") return("https://www.landesdatenbank.nrw.de/ldbnrwws/services/")
+    if (db=="regio") return("https://www.regionalstatistik.de/genesisws/services/")
+    if (db=="de") return("https://www-genesis.destatis.de/genesisWS/web/")
+    if (db=="bm") return("https://www.bildungsmonitoring.de/bildungws/services/")
+    if (db=="st") return("https://genesis.sachsen-anhalt.de/webservice/services/")
+    if (db=="by") return("https://www.statistikdaten.bayern.de/genesisWS/services/")
+    stop("DB: Currently not implemented.")
+  }
+  }
 
 set_db2 <- function(db){
 	if (db=="de") return("https://www-genesis.destatis.de/genesis/online")
 	if (db=="by") return("https://www.statistikdaten.bayern.de/genesis/online")
-	if (db=="regio") return("https://www.regionalstatistik.de/genesis/online/")	
+	if (db=="regio") return("https://www.regionalstatistik.de/genesis/online/")
 	stop("DB: Currently not implemented.")
 	}
 
 
-get_character_vec <- function(x){ 
+get_character_vec <- function(x){
 	x <- paste(unlist(na.omit(x), use.names=FALSE), collapse="_")
 	x <- stri_trans_general(x, "Latin-ASCII")
 	x <- str_replace_all(x, " *", "")
--- R/test_login.R
+++ R/test_login.R
@@ -1,41 +1,61 @@
-#' Tests Login in GENESIS Databases 
+#' Tests Login in GENESIS Databases
 #'
-#' \code{test_login} tests if the login works. 
-#' 
+#' \code{test_login} tests if the login works.
+#'
 #'
 #' @param genesis to authenticate a user and set the database (see below).
-#' @param ... other arguments send to the httr::GET request. 
-#' 
-#'  
-#' @return a \code{string} with the server return message. 
-#'  
-#' 
+#' @param restapi use RESTful/JSON Webservice if implemented (default FALSE)
+#' @param ... other arguments send to the httr::GET request.
 #'
-#' @examples 
-#' 
+#'
+#' @return a \code{string} with the server return message.
+#'
+#'
+#'
+#' @examples
+#'
 #'  \dontrun{
-#' 
+#'
 #'  test_login(genesis=c(db="regio") )
-#' 
+#'
 #'  }
-#' 
-#' 
-#' 
-#' 
+#'
+#'
+#'
+#'
 #' @export
-test_login <- function(genesis=NULL, ... ) {
+test_login <- function(genesis=NULL, restapi = FALSE, ... ) {
 
-	genesis <- make_genesis(genesis)
+  genesis <- make_genesis(genesis)
 
-	baseurl <- paste(set_db(db=genesis['db']), "TestService_2010", sep="")
+  if(restapi){
 
-	param <- list(
-		method  = 'logonoff',
-		kennung  = genesis['user'],
-		passwort = genesis['password'])
+    baseurl <- paste(set_db(db=genesis['db'], restapi), "helloworld/logincheck", sep="")
 
-	httrdata <- GET(baseurl, query  = param, ... )
-	xmldata <- content(httrdata, type='text/xml', encoding="UTF-8")
+    param <- list(
+      username  = genesis['user'],
+      password = genesis['password'],
+      language = "de")
 
-	return(xml_text(xmldata))
+    httrdata <- GET(baseurl, query  = param )
+    jsondata <- content(httrdata, type='application/json', encoding="UTF-8")
+
+    return(paste(jsondata$Status, "(restapi)"))
+
+  } else {
+
+    baseurl <- paste(set_db(db=genesis['db']), "TestService_2010", sep="")
+
+    param <- list(
+      method  = 'logonoff',
+      kennung  = genesis['user'],
+      passwort = genesis['password'])
+
+    httrdata <- GET(baseurl, query  = param, ... )
+    xmldata <- content(httrdata, type='text/xml', encoding="UTF-8")
+
+    return(xml_text(xmldata))
+
+  }
+
 	}
